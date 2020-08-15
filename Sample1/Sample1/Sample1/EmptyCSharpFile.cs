c04d6637cbbfbf0f6e99f137ad203649df829d1a
菊地弥恵 <yaekikuchi@kikuchiwatarumeguminoMacBook-ea.local>
2020-08-15 16:56:25 +0900

    first commit

---
+++ Sample1/Sample1/Sample1/EmptyCSharpFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+
+class Sample1
+{
+    public static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("ようこそC#へ！");
+    }
+
+}
